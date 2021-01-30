14d07a0336f3d552ee5b2acd2ee772cc2d583c1d
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2021-01-30 11:56:03 +0100

    update to symfony static dumper project

---
+++ src/Controller/AppController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('app/about.html.twig');
+    }
+
+    /**
+     * @Route("/purchase", name="purchase")
+     */
+    public function purchase(): Response
+    {
+        return $this->render('app/purchase.html.twig');
+    }
+
+
+}
